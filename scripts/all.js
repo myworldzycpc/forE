aed97624fbcc9f90de416f5f8dc282d40544d907
myworldzycpc <67212814+myworldzycpc@users.noreply.github.com>
2020-07-05 10:45:44 +0800

    Create all.js

---
+++ scripts/all.js
@@ -0,0 +1,3 @@
+function urlbtn(url, text){
+  document.write('<a href="' + url + '"><div style="border: 2px solid #379082;border-radius: 20px;padding: 30px 30px;text-align:center;color: #00FF00;background-color:rgba(255,0,0,0.5)">' + text + '</div></a>'
+}
