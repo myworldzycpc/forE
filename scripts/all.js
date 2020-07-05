97208368cb4f1683d04b847547f4dd702da1769c
myworldzycpc <67212814+myworldzycpc@users.noreply.github.com>
2020-07-05 10:55:33 +0800

    Update all.js

--- scripts/all.js
+++ scripts/all.js
@@ -1,3 +1,4 @@
 function urlbtn(url, text){
+  window.alert("btnload")
   document.write('<a href="' + url + '"><div style="border: 2px solid #379082;border-radius: 20px;padding: 30px 30px;text-align:center;color: #00FF00;background-color:rgba(255,0,0,0.5)">' + text + '</div></a>'
 }
